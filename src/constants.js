066c05e7d412fbc8286e1b54b27ac97c4a2875ee
zorawar1090 <zorawarsingh9988@gmail.com>
2019-09-19 16:40:48 +0200

    imports contants file

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-//export const url = 'https://localhost:4000/'
-export const baseUrl = 'https://peaceful-badlands-01023.herokuapp.com/'
+//export const url = 'https://localhost:4000'
+const baseUrl = 'https://peaceful-badlands-01023.herokuapp.com'
+
+export default baseUrl
